3b6b74f221ace4f243d1ba26a2a832a82ca3ca4a
Nickie <kankuro.mat@outlook.com>
2021-10-05 15:40:23 -0300

    Second Commit

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -2,6 +2,7 @@
    <div class="user-admin">
       <b-form>
          <input id="user-id" type="hidden" v-model="user.id" />
+
          <b-row>
             <b-col md="6" sm="12">
                <b-form-group label="Nome:" label-for="user-name">
@@ -11,6 +12,7 @@
                      placeholder="Informe o Nome do Usuário..." />
                </b-form-group>
             </b-col>
+
             <b-col md="6" sm="12">
                <b-form-group label="E-mail:" label-for="user-email">
                   <b-form-input id="user-email" type="text"
@@ -20,10 +22,12 @@
                </b-form-group>
             </b-col>
          </b-row>
+
          <b-form-checkbox id="user-admin" v-show="mode === 'save'" 
             v-model="user.admin" class="mt-3 mb-3">
             Administrador?
          </b-form-checkbox>
+
          <b-row v-show="mode === 'save'">
             <b-col md="6" sm="12">
                <b-form-group label="Senha:" label-for="user-password">
@@ -32,6 +36,7 @@
                      placeholder="Informe a Senha do Usuário..." />
                </b-form-group>
             </b-col>
+
             <b-col md="6" sm="12">
                <b-form-group label="Confirmação de Senha:"
                label-for="user-confirm-password">
@@ -41,15 +46,30 @@
                </b-form-group>
             </b-col>
          </b-row>
-         <b-button variant="primary" v-if="mode === 'save'"
-            @click="save">Salvar</b-button>
-         <b-button variant="danger" v-if="mode === 'remove'"
-            @click="remove">Excluir</b-button>
-         <b-button class="ml-2" @click="reset">Cancelar</b-button>
+
+         <b-row>
+            <b-col xs="12">
+               <b-button variant="primary" v-if="mode === 'save'"
+                  @click="save">Salvar</b-button>
+               <b-button variant="danger" v-if="mode === 'remove'"
+                  @click="remove">Excluir</b-button>
+               <b-button class="ml-2" @click="reset">Cancelar</b-button>
+            </b-col>   
+         </b-row>
+         
       </b-form>
       <hr>
 
-     <b-table hover striped :items="users" :fields="fields"></b-table>
+      <b-table hover striped :items="users" :fields="fields">
+         <template slot="actions" slot-scope="data">
+                <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-button>
+         </template>       
+     </b-table>
    </div>
 </template>
 
--- frontend/src/config/global.js
+++ frontend/src/config/global.js
@@ -4,9 +4,11 @@ export const baseApiUrl = 'http://localhost:3000'
 
 export function showError(e) {
    if(e && e.response && e.response.data) {
-      Vue.tosted.global.defaultError({ msg : e.response.data })
+      Vue.toasted.global.defaultError({ msg : e.response.data })
    } else if(typeof e === 'string') {
-      Vue.tosted.global.defaultError()
+      Vue.toasted.global.defaultError({ msg : e })
+    } else {
+      Vue.toasted.global.defaultError()
    }
 }
 
